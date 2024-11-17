a00413ac91046eb6facd3ed74e572c9798a34cf0
FA <furqan@script.tv>
2024-11-17 23:08:06 +0200

    fix typo

--- core/license.go
+++ core/license.go
@@ -214,7 +214,7 @@ func ValidateLicense(licensee common.Address) error {
 	}
 	if !signature.Verify(dataToValidate, license.Issuer) {
 		verifiedLicenseCache[licensee] = false
-		return fmt.Errorf("invalid license signature: %v, %v, %v, %v, %v, %v, %v", hex.EncodeToString(dataToValidate), hex.EncodeToString(keccak256(dataToValidate)), license.Issuer.Hex(), base64.StdEncoding.EncodeToString(signature.ToBytes()), license.From, license.To, concatenatedData)	
+		return fmt.Errorf("invalid license signature: %v, %v, %v, %v, %v, %v, %v", hex.EncodeToString(dataToValidate), hex.EncodeToString(keccak256(dataToValidate)), license.Issuer.Hex(), base64.StdEncoding.EncodeToString(signature.ToBytes()), license.From, license.To, dataToValidate)	
 	}
 
 	// cache the verified status
