2d3242f26d611a60f61fa905395b71ed016509fd
FA <furqan@script.tv>
2024-12-15 13:30:16 +0200

    lpbh init fix

--- consensus/engine.go
+++ consensus/engine.go
@@ -184,7 +184,7 @@ func (e *ConsensusEngine) Start(ctx context.Context) {
 
 	e.checkSyncStatus()
 
-	e.lastProcessedBlockHeight = e.state.GetLastFinalizedBlock()
+	e.lastProcessedBlockHeight = e.state.GetLastFinalizedBlock().Height
 	e.wg.Add(1)
 	go e.mainLoop()
 }
